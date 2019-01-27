7b167f7a88f4d106f92b7ec55e2f3e4201cb05b1
Rupert Broad <rupert.broad@gmail.com>
2019-01-27 11:58:17 +0100

    add ecr repo

--- main.tf
+++ main.tf
@@ -3,7 +3,9 @@ provider "github" {
   organization = "${var.github_organization}"
 }
 
-# module "terraform" {
-#   source = "./terraform"
-# }
+module "terraform" {
+  source = "./terraform"
 
+  github_token   = "${var.github_token}"
+  circleci_token = "${var.circleci_token}"
+}
