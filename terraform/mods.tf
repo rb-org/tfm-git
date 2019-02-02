6af844976744e84d5975cf6e52f73d963489e96d
Rupert Broad <rupert.broad@gmail.com>
2019-02-02 11:57:50 +0100

    add mods lb misc

--- terraform/mods.tf
+++ terraform/mods.tf
@@ -52,12 +52,66 @@ module "tfm_aws_ec2" {
   github_token    = "${var.github_token}"
 }
 
-# EC2 Default
+# EBS 
 module "tfm_aws_ebs_vol" {
   source = "github.com/rb-org/terraform-aws-rb-github?ref=v0.0.3"
 
   repo_name        = "${var.name_prefix}-aws-mod-ebs"
   repo_description = "${var.desc_prefix} - Module - EBS Volumes"
+  license_template = "mit"
+  private_repo     = false
+  github_org       = "${var.github_organization}"
+  team_count       = 1
+
+  teams = "${list(
+    map("team", var.gh_team_id_admins, "perms", "admin"),
+  )}"
+
+  # Master branch protection
+  enable_branch_protection = true
+  enforce_admins           = false
+  strict_status_checks     = true
+  contexts                 = ["ci/circleci: lint_validate"]
+
+  # CircleCI
+  update_circleci = true
+  circleci_token  = "${var.circleci_token}"
+  github_token    = "${var.github_token}"
+}
+
+# Load Balancers 
+module "tfm_aws_lb" {
+  source = "github.com/rb-org/terraform-aws-rb-github?ref=v0.0.4"
+
+  repo_name        = "${var.name_prefix}-aws-mod-lb"
+  repo_description = "${var.desc_prefix} - Module - Load Balancers"
+  license_template = "mit"
+  private_repo     = false
+  github_org       = "${var.github_organization}"
+  team_count       = 1
+
+  teams = "${list(
+    map("team", var.gh_team_id_admins, "perms", "admin"),
+  )}"
+
+  # Master branch protection
+  enable_branch_protection = true
+  enforce_admins           = false
+  strict_status_checks     = true
+  contexts                 = ["ci/circleci: lint_validate"]
+
+  # CircleCI
+  update_circleci = true
+  circleci_token  = "${var.circleci_token}"
+  github_token    = "${var.github_token}"
+}
+
+# Load Balancers 
+module "tfm_aws_misc" {
+  source = "github.com/rb-org/terraform-aws-rb-github?ref=v0.0.4"
+
+  repo_name        = "${var.name_prefix}-aws-mod-misc"
+  repo_description = "${var.desc_prefix} - Module - Misc"
   license_template = "mit"
   private_repo     = false
   github_org       = "${var.github_organization}"
