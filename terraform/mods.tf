d04bca7147e80df7428e3cccff6ef25af1058e5f
Rupert Broad <rupert.broad@gmail.com>
2019-02-11 21:10:33 +0100

    remove ecs and mod-ecs

--- terraform/mods.tf
+++ terraform/mods.tf
@@ -132,3 +132,31 @@ module "tfm_aws_misc" {
   circleci_token  = "${var.circleci_token}"
   github_token    = "${var.github_token}"
 }
+
+# ECS
+
+module "tfm_ecs" {
+  source = "github.com/rb-org/terraform-aws-rb-github?ref=v0.0.4"
+
+  repo_name        = "${var.name_prefix}-aws-mod-ecs"
+  repo_description = "${var.desc_prefix} - Module ECS"
+  license_template = "mit"
+  private_repo     = false
+  github_org       = "${var.github_organization}"
+  team_count       = 1
+
+  teams = "${list(
+    map("team", var.gh_team_id_admins, "perms", "admin"),
+  )}"
+
+  # Master branch protection
+  enable_branch_protection = true
+  enforce_admins           = false
+  strict_status_checks     = true
+  contexts                 = ["ci/circleci: lint_validate"]
+
+  # CircleCI
+  update_circleci = true
+  circleci_token  = "${var.circleci_token}"
+  github_token    = "${var.github_token}"
+}
