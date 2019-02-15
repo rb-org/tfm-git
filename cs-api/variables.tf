f291eaace47979dd18a92bac8e35d9ad396d51ae
Rupert Broad <rupert.broad@gmail.com>
2019-02-15 18:07:14 +0100

    add cs-api

---
+++ cs-api/variables.tf
@@ -0,0 +1,14 @@
+# Variables
+
+variable "name_prefix" {}
+
+variable "desc_prefix" {}
+
+variable "github_token" {}
+variable "circleci_token" {}
+
+variable "github_organization" {
+  default = ""
+}
+
+variable "gh_team_id_admins" {}
--- main.tf
+++ main.tf
@@ -11,3 +11,14 @@ module "terraform" {
   github_organization = "${var.github_organization}"
   gh_team_id_admins   = "${data.github_team.admins.id}"
 }
+
+module "cs-api" {
+  source = "./cs-api"
+
+  name_prefix         = "cs-api"
+  desc_prefix         = "Terraform - CS API"
+  github_token        = "${var.github_token}"
+  circleci_token      = "${var.circleci_token}"
+  github_organization = "${var.github_organization}"
+  gh_team_id_admins   = "${data.github_team.admins.id}"
+}
