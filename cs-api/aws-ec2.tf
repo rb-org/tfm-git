ef3d8ce3ac92d322a6ff8ad30f40d6c623ac7a28
Rupert Broad <rupert.broad@gmail.com>
2019-02-15 22:27:20 +0100

    update context

--- cs-api/aws-ec2.tf
+++ cs-api/aws-ec2.tf
@@ -16,7 +16,7 @@ module "tfm_aws_ec2" {
   enable_branch_protection = true
   enforce_admins           = false
   strict_status_checks     = true
-  contexts                 = ["ci/circleci: feat_plan"]
+  contexts                 = ["ci/circleci: featdev_plan"]
 
   # CircleCI
   update_circleci = true
