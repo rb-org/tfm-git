a4db21bc28ee80d25cff2169d438d6c889c10362
Rupert Broad <rupert.broad@gmail.com>
2019-03-10 16:30:06 +0100

    add eks dash to cs api

--- cs-api/aws-eks-dash.tf
+++ cs-api/aws-eks-dash.tf
@@ -1,8 +1,8 @@
-module "tfm_eks_dashboard" {
-  source = "github.com/rb-org/terraform-aws-rb-github?ref=v0.0.4"
+module "tfm_aws_eks" {
+  source = "github.com/rb-org/terraform-aws-rb-github?ref=v0.0.6"
 
   repo_name        = "${var.name_prefix}-aws-eks-dashboard"
-  repo_description = "${var.desc_prefix} - EKS Dashboard"
+  repo_description = "${var.desc_prefix} - AWS EKS Dashboard"
   license_template = "mit"
   private_repo     = false
   github_org       = "${var.github_organization}"
@@ -16,7 +16,7 @@ module "tfm_eks_dashboard" {
   enable_branch_protection = true
   enforce_admins           = false
   strict_status_checks     = true
-  contexts                 = ["ci/circleci: feat_plan"]
+  contexts                 = ["ci/circleci: feat2dev_plan"]
 
   # CircleCI
   update_circleci = true
